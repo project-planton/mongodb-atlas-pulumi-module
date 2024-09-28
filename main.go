190f83ad055ea334e4740a3a8da2fc1510a3af35
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-28 13:19:59 +0530

    renamed planton to project-planton

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/mongodb-atlas-pulumi-module/pkg"
-	"github.com/plantoncloud/planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/atlas/mongodbatlas"
-	_ "github.com/plantoncloud/planton/apis/zzgo/cloud/planton/apis/iac/v1/stackjob"
+	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/atlas/mongodbatlas"
+	_ "github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/iac/v1/stackjob"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
--- pkg/main.go
+++ pkg/main.go
@@ -1,7 +1,7 @@
 package pkg
 
 import (
-	"github.com/plantoncloud/planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/atlas/mongodbatlas"
+	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/atlas/mongodbatlas"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
