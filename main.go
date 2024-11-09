aa4426cc9144f2cb915e1e67f60bba9219fa2733
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-11-09 17:57:19 +0530

    switched over to using local generated stubs for project-planton apis

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	mongodbatlasv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/atlas/mongodbatlas/v1"
 	"github.com/pkg/errors"
 	"github.com/project-planton/mongodb-atlas-pulumi-module/pkg"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
+	mongodbatlasv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/atlas/mongodbatlas/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
--- pkg/locals.go
+++ pkg/locals.go
@@ -1,7 +1,7 @@
 package pkg
 
 import (
-	mongodbatlasv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/atlas/mongodbatlas/v1"
+	mongodbatlasv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/atlas/mongodbatlas/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
